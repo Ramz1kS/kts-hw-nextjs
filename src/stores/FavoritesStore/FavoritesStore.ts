ee55de3dc03a894637cffee65870cf505414afb8
Ramz1kS <skib1dimin1on2006@gmail.com>
2026-03-10 19:59:39 +0300

    unnecessary loading in favoritesstore removed

--- src/stores/FavoritesStore/FavoritesStore.ts
+++ src/stores/FavoritesStore/FavoritesStore.ts
@@ -14,7 +14,6 @@ const E_COMMERSE_STORAGE_NAME = "ecommerse_favorites";
 
 class FavoritesStore {
   productIds: number[] = [];
-  products: ProductData[] = [];
   loadingInfo: LoadingInfo = {
     isLoading: false,
     isError: false,
@@ -28,7 +27,6 @@ class FavoritesStore {
     this.rootStore = root;
     makeObservable(this, {
       productIds: observable,
-      products: observable,
       loadingInfo: observable,
       isHydrated: observable,
       toggleProduct: action.bound,
@@ -36,7 +34,6 @@ class FavoritesStore {
       addProductId: action.bound,
       includes: action.bound,
       clear: action.bound,
-      loadProducts: action.bound,
       hydrate: action.bound,
       count: computed,
     });
@@ -78,49 +75,10 @@ class FavoritesStore {
 
   removeProductId(id: number) {
     const index = this.productIds.indexOf(id);
-    if (index > -1) {
-      this.products = this.products.filter((item) => item.id !== id);
-      this.productIds.splice(index, 1);
-    }
   }
 
   clear() {
     this.productIds = [];
-    this.products = [];
-  }
-
-  async loadProducts() {
-    if (this.productIds.length === 0) {
-      this.products = [];
-      this.loadingInfo.isLoading = false;
-      return;
-    }
-
-    this.loadingInfo.isLoading = true;
-
-    try {
-      const uniqueIds = [...new Set(this.productIds)];
-      const res = await fetch(apiPaths.getProductsByIds(uniqueIds));
-
-      if (!res.ok) {
-        throw new Error(res.status.toString());
-      }
-
-      const data: ListResponse = await res.json();
-
-      runInAction(() => {
-        this.products = data.data;
-      });
-    } catch (e) {
-      runInAction(() => {
-        this.loadingInfo.isError = true;
-        this.loadingInfo.errorCode = e instanceof Error ? e.message : "200";
-      });
-    } finally {
-      runInAction(() => {
-        this.loadingInfo.isLoading = false;
-      });
-    }
   }
 
   get count() {
